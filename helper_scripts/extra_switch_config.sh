a80d9248cc394d0a6eea5fb300b535b47d737f16
Pete Lumbis <plumbis@cumulusnetworks.com>
2018-04-27 13:28:43 -0700

    update helper scripts for lab

--- helper_scripts/extra_switch_config.sh
+++ helper_scripts/extra_switch_config.sh
@@ -11,16 +11,10 @@ cat <<EOT > /etc/network/interfaces
 auto lo
 iface lo inet loopback
 
-auto vagrant
-iface vagrant inet dhcp
-
 auto eth0
 iface eth0 inet dhcp
 
 EOT
-
-#add line to support bonding inside virtualbox VMs
-#sed -i '/.*iface swp.*/a\    #required for traffic to flow on Bonds in Vbox VMs\n    post-up ip link set $IFACE promisc on' /etc/network/interfaces
 
 echo "#################################"
 echo "   Finished"
